5bc88e91b511a7091124057a2c4d2680e05de7d2
Kevin Menhinick <kamenhinick@gmail.com>
2017-10-05 09:52:40 +1300

    Start Bluetooth device class

---
+++ Assets/scripts/BluetoothDevice.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluetoothDevice {
+
+    private SphereCollider connectionCollider;
+
+    public BluetoothDevice(GameObject gameObject, float connectionRange)
+    {
+        connectionCollider = gameObject.AddComponent<SphereCollider>();
+        connectionCollider.center = Vector3.zero;
+        connectionCollider.radius = connectionRange;
+    }
+}
--- Assets/scripts/MasterBehaviour.cs
+++ Assets/scripts/MasterBehaviour.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class MasterBehaviour : MonoBehaviour {
 
 	public GameObject This;
+    public float ConnectionRange = 5.0f;
+
+    private BluetoothDevice bluetoothDevice;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log (This.name + " started");
-	}
+        bluetoothDevice = new BluetoothDevice(This, ConnectionRange);
+    }
 	
 	// Update is called once per frame
 	void Update () {
