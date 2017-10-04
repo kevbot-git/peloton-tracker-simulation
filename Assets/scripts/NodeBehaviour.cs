b92cfaba6864dd584f839b53c98200528488b582
Kevin Menhinick <kamenhinick@gmail.com>
2017-10-05 00:29:09 +1300

    Add base master and node device prefabs

---
+++ Assets/scripts/NodeBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeBehaviour : MonoBehaviour {
+
+	public GameObject This;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (This.name + " started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
